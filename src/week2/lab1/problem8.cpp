644bd82a00fd924fd53644abceaa1bf52381ff5a
ashuralieyeev <bubbleice77@gmail.com>
2025-06-09 18:57:02 +0500

    changed problem 8(w2l1)

--- src/week2/lab1/problem8.cpp
+++ src/week2/lab1/problem8.cpp
@@ -1,33 +1,44 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int x;
-    cout<<"enter 4 digit number"<<endl;
-    cin>>x;
+int main() {
+    int num;
+    cin >> num;
     int a,b,c,d;
-    a=x%10;
-    x/=10;
-    b=x%10;
-    x/=10;
-    c=x%10;
-    x/=10;
-    d=x%10;
-    int odd=4;
+    a=num%10;
+    num/=10;
+    b=num%10;
+    num/=10;
+    c=num%10;
+    num/=10;
+    d=num%10;
+    int odd=0;
     int even=0;
-    if (a % 2 == 0)
-        even++; else odd++;
-    if (b % 2 == 0)
-        even++; else odd++;
-    if (c % 2 == 0)
-        even++; else odd++;
-    if (d % 2 == 0)
-        even++; else odd++;
 
+    if (a%2==0){
+        even++;
+    }else{
+        odd ++;
+    }
+    if (b%2==0) {
+        even++;
+    }else{
+        odd++;
+    }
+    if (c % 2 == 0){
+        even++;
+    }else {
+        odd++;
+    }
+    if (d % 2 == 0){
+        even++;
+    }else {
+        odd++;
+    }
     if (even > odd) {
         cout << "More even digits" << endl;
-       } else if (odd > even) {
+    } else if (odd > even) {
         cout << "More odd digits" << endl;
-       } else {
+    } else {
         cout << "Equal even and odd digits" << endl;
     }
 
